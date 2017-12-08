5409952696306fe2a531c70a252a40ebbb9c84d2
JonasJs <joonasjss@gmail.com>
2017-12-08 02:45:44 -0200

    Added gulp

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <div id="app">
+  <section id="app">
     <img src="./assets/logo.png">
     <router-view/>
-  </div>
+  </section>
 </template>
 
 <script>
-import { eventRef } from '@/firebase/firebase';
+
 export default {
   name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
